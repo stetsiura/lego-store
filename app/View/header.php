fda2d826179fa850f150dcbb9d58c2a682ad9172
Aleksey Stetsyura <a.y.stetsyura@outlook.com>
2019-01-13 21:06:21 +0200

    Подключение шрифта

--- app/View/header.php
+++ app/View/header.php
@@ -16,8 +16,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i&amp;subset=cyrillic-ext" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
     <?php Asset::render('css'); ?>
     <link rel="shortcut icon" type="image/x-icon" href="/app/assets/img/favicons/favicon.ico">
     <link rel="icon" type="image/png" href="/app/assets/img/favicons/favicon-32.png" sizes="32x32">
--- db_seed.sql
+++ db_seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO user (email, password, role, name, register_date) VALUES ('a.y.stetsyura@outlook.com', 'acf2d89c77e8af6d8de1bb7b86abcbd7', 'admin', 'Алексей Стецюра', '2019-01-08 18:19:54');
 
-INSERT INTO setting (setting_key, name, value) VALUES ('SUPPORT_EMAIL', 'E-mail администратора', 'a.y.stetsyura@outlook.com');
+INSERT INTO setting (setting_key, name, value) VALUES ('support-email', 'E-mail администратора', 'a.y.stetsyura@outlook.com');
 
 INSERT INTO category (id, creation_date, alias, original_name, name) VALUES (1, '2018-01-08 18:19:54', 'unsorted', 'Unsorted sets', 'Несортированные товары');
 

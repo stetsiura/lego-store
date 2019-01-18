1a769fd14e089704432dd82b34c0fa633acce57a
Aleksey Stetsyura <a.y.stetsyura@outlook.com>
2019-01-18 20:05:36 +0200

    Готов футер

--- app/View/footer.php
+++ app/View/footer.php
@@ -1,3 +1,33 @@
+<footer class="footer padding-b-40">
+    <div class="container">
+        <div class="row clearfix">
+            <div class="left-side">
+                <div class="logo">
+                    <img src="/app/assets/img/common/logo-white.png" alt="BricksUnity logo">
+                </div>
+                <div class="menu">
+                    <span>&COPY; <?php echo date("Y"); ?> BricksUnity</span>
+                    <ul>
+                        <li><a href="#">Каталог</a></li>
+                        <li><a href="#">Темы</a></li>
+                        <li><a href="#">Блог</a></li>
+                        <li><a href="#">Служба поддержки</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="right-side">
+                <h6>Подписаться на расслыку</h6>
+                <div class="email-subscription">
+                    <form method="POST">
+                        <input type="text" name="email" id="subscription-email" class="subscription-email" placeholder="Ваш E-mail..." /><button type="submit" class="subscription-btn">@</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
 <div id="notification" class="notification success">
     <i class="fa fa-check-circle"></i>
     <div id="notification-content" class="notification-content">
